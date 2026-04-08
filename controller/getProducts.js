b431b6cbdfec7e5e3a3d789730fa2c946dfee18a
Samay Gaihar <time58734@gmail.com>
2026-04-08 23:53:45 +0530

    Improved and added route as well as controller for getting a specific product

--- controller/getProducts.js
+++ controller/getProducts.js
@@ -1,5 +1,7 @@
 import user from "../model/ProductModel.js";
+
 async function getProduct(req,res){
+    // To get all the products by GET method
 try{
     
     let response= await user.find({})
@@ -12,6 +14,7 @@ catch(err){
 }
 }
 function createProduct(req,res){
+    //To create a product
     let {product_name,description,price}=req.body;
     let response=user.create({
         product_name,
@@ -20,4 +23,25 @@ function createProduct(req,res){
     })
 return res.status(200).json(response)
 }
-export default {getProduct,createProduct}
+// "69d692f9d85245ed0194670d"
+ async function specificProduct(req,res){
+    // To get a single product
+    try{
+let {id}=req.params;
+let response= await user.findById(id)
+if(!response){
+    return res.status(404).json({message:"Sorry the product did not found"})
+}
+else{
+return res.status(200).json(response)
+}
+    }
+    catch(err) {
+    return res.status(500).json({
+      message: "Invalid product ID or server error"
+    });
+}
+ }
+
+export default {getProduct,createProduct,specificProduct}
+
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ function route1(app){
 	// `getProducts` is exported as the default function, so use it directly
 	app.get('/product', getProducts.getProduct)
     app.post('/createProduct',getProducts.createProduct)
+    app.get('/product/:id',getProducts.specificProduct)
 }
 export default route1
